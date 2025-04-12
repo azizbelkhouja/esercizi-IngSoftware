dc5ba604df2bbf0f26557b3b66760ffb6e0b494b
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-04-12 11:54:21 +0200

    Esercizio 21: Filtro Numeri Pari (Array + if)

--- Esercizio 21/Main.java
+++ Esercizio 21/Main.java
@@ -1,5 +1,14 @@
-package Esercizio 21;
-
 public class Main {
     
+    public static void main(String[] args) {
+        
+        int[] array = {1, 2, 3, 4, 5, 22, 59, 47, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+        for (int numero : array) {
+
+            if (numero % 2 == 0) {
+                System.out.println(numero + " è pari");
+            }
+        }
+    }
 }
