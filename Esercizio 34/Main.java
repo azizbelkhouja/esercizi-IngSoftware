78491a6c9b0eded216267f7c55269f762258d950
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-04-12 12:47:15 +0200

    Esercizio 34: Ordina Parole (args + Arrays.sort)

--- Esercizio 34/Main.java
+++ Esercizio 34/Main.java
@@ -1,5 +1,18 @@
-package Esercizio 34;
+import java.util.Arrays;
 
 public class Main {
-    
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Nessuna parola fornita.");
+            return;
+        }
+
+        Arrays.sort(args);
+
+        System.out.println("Parole ordinate alfabeticamente:");
+        for (String parola : args) {
+            System.out.println(parola);
+        }
+    }
 }
